a95a839fdcd33277c9c64c44a1485075f0a7c2e8
Maya Kannan <mayakannan@ucla.edu>
2023-04-28 13:37:25 -0700

    Test commit.

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          This is a sample commit. Please ignore. 
         </p>
         <a
           className="App-link"
