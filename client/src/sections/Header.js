55231b3fe080338e1dea9ad512050b300ebbc34b
Maya Kannan <mayakannan@ucla.edu>
2023-05-06 22:23:29 -0700

    Added themes and fonts.

--- client/src/sections/Header.js
+++ client/src/sections/Header.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
 
 function Header() {
     return (
       <>
         <Typography variant="h1">
-            eventer
+            eventer.
         </Typography>
         <Typography variant="h3">
             Discover LA
