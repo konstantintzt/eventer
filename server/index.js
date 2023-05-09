a86e0b157bfe6be7ab23212cae9f528f3997de8c
Sean Tang <seantang001@g.ucla.edu>
2023-05-08 19:36:11 -0700

    started newEvent

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const { MongoClient } = require("mongodb")
+const { joi } = require("joi")
 
 dotenv.config()
 
 const app = express()
 
+app.use(express.json())
 
 async function main() {
     const mongoURI = process.env.MONGO_URI
@@ -18,12 +20,32 @@ async function main() {
         console.error(e)
         process.exit(-1)
     } finally {
-        await mongoClient.close()
     }
     app.listen(process.env.PORT, () => {
         console.log("API is running on port " + process.env.PORT)
         app.get("/", (req, res) => {
             res.send("Hello World!")
+        })
+        app.post("/new", async (req, res) => {
+            try{
+                let newEvent = req.body
+                const db = mongoClient.db("eventer-dev")
+                const events = db.collection("events")
+                const doc = {
+                    organizer: newEvent["organizer"],
+                    address: newEvent["address"],
+                    zip: newEvent["zip"],
+                    type: newEvent["type"],
+                    date: newEvent["date"],
+                    desc: newEvent["desc"]
+                }
+                console.log(doc)
+                console.log(newEvent)
+                await events.insertOne(doc)
+            }
+            finally {
+                res.sendStatus(200)
+            }
         })
     })
 
