f921f011ca68a06d2f6d25afc09216e517c91ad1
konstantintzt <49292114+konstantintzt@users.noreply.github.com>
2023-05-10 18:09:49 -0700

    Added API Endpoint: Fetching a single event based on its UUID
    Every single event inserted in the database will have a server-generated UUID. This endpoint allows the client to access information about a specific event based on this UUID. For now, the information sent back from this endpoint is identical to the information sent back by the global event fetching endpoint. In the future, however, this new endpoint will provide more detailed information than the global one.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv")
-const { fetchEventsSchema } = require("./querySchemas")
+const { fetchEventsSchema, fetchSingleEventSchema } = require("./querySchemas")
 const { MongoClient } = require("mongodb")
 
 dotenv.config()
@@ -24,13 +24,22 @@ async function main() {
     }
     app.get("/events", async (req, res) => {
         const { error, value } = fetchEventsSchema.validate(req.query)
-        if (error) return res.status(400).json( {error: error.details[0].message})
+        if (error) return res.status(400).json({ error: error.details[0].message })
         else {
             if (value.before != undefined) value.date = { $lte: value.before }
-            if (value.after != undefined) value.date = {...value.date, $gte: value.after }
+            if (value.after != undefined) value.date = { ...value.date, $gte: value.after }
             value.before = undefined
             value.after = undefined
             const results = await database.collection("events").find(value).toArray()
+            return res.status(200).json(results)
+        }
+    })
+
+    app.get("/event/:uuid", async (req, res) => {
+        const { error, value } = fetchSingleEventSchema.validate(req.params)
+        if (error) return res.status(400).json( { error: error.details[0].message } )
+        else {
+            const results = await database.collection("events").findOne(value)
             return res.status(200).json(results)
         }
     })
