e81ea38cb103a9ab79f798fffd2666c5b3ae0bed
konstantintzt <49292114+konstantintzt@users.noreply.github.com>
2023-05-13 22:32:25 -0700

    Restructured the back-end to make it more modular
    Separated database connection logic from routing logic. Additionally, separated the logic that handles each route from the main routing logic.

---
+++ server/routes/attend.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const database = require("../database")
+const { addEventAttendeeBodySchema } = require("../requestSchemas")
+
+const router = express.Router()
+
+router.post("/", async (req, res) => {
+    const { error, value } = addEventAttendeeBodySchema.validate(req.body)
+    if (error) return res.status(400).json({ error: error.details[0].message })
+    else {
+        const event = await database.getDB().collection("events").findOne({ uuid: value.uuid })
+        if (event == null) return res.status(400).json({ error: "Event does not exist" })
+        else {
+            await database.getDB().collection("attendances").insertOne({...value, confirmed: Math.floor(Date.now()/1000)})
+            return res.status(200).json({ success: true })
+        }
+    }
+})
+
+module.exports = router
