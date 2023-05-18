1621f050b9e3a1a20f920f3b58d4f006f14b7f9c
Sean Tang <seantang001@g.ucla.edu>
2023-05-17 22:25:30 -0700

    removed debug messages

--- server/routes/event.js
+++ server/routes/event.js
@@ -10,12 +10,12 @@ router.get("/:uuid", async (req, res) => {
     const { error, value } = fetchSingleEventParamsSchema.validate(req.params)
     if (error) return res.status(400).json({ error: error.details[0].message })
     else {
-        console.log("uuid")
-        console.log(value)
+        // console.log("uuid")
+        // console.log(value)
         const attendees = await database.getDB().collection("attendances").find(value).toArray()
         const results = await database.getDB().collection("events").findOne(value)
-        console.log(results)
-        console.log(attendees)
+        // console.log(results)
+        // console.log(attendees)
         return res.status(200).json({event: results, attendees: attendees})
     }
 })
@@ -24,7 +24,7 @@ router.post("/new", async (req, res) => {
     const { error, value } = addNewEventBodySchema.validate(req.body)
     if (error) return res.status(400).json({ error: error.details[0].message })
     else {
-        console.log(req.body)
+        // console.log(req.body)
         await database.getDB().collection("events").insertOne({...value, uuid: uuidv4()})
         return res.status(200).json({ success: true })
     }
