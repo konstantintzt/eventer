b51203c825464ddfeae7cfb59ad70fd7233bf5be
Maya Kannan <mayakannan@ucla.edu>
2023-05-26 00:14:49 -0700

    Fixed MUI theme bug.

--- client/src/Themes.js
+++ client/src/Themes.js
@@ -1,54 +1,55 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 
 export const theme = createTheme({
-  palette: {
-    mode: 'light',
-    colors: {
-      main: '#3f51b5',
-      red: '#d0312d',
-      white: '#ffffff'
+    palette: {
+      mode: 'light',
+      colors: {
+        main: '#3f51b5',
+        red: '#d0312d',
+        white: '#ffffff'
+      },
     },
-  },
-  typography: {
-    h1: {
-      fontFamily: 'Montserrat',
-      fontWeight: 800,
+    typography: {
+      h1: {
+        fontFamily: 'Montserrat',
+        fontWeight: 800,
+        color: '#FF0000'
+      },
+      h2: {
+        fontFamily: 'Montserrat',
+      },
+      h3: {
+        fontFamily: 'Montserrat',
+      },
+      h4: {
+        fontFamily: 'Montserrat',
+      },
+      h5: {
+        fontFamily: 'Montserrat',
+      },
+      h6: {
+        fontFamily: 'Montserrat',
+      },
+      subtitle1: {
+        fontFamily: 'Hind',
+      },
+      subtitle2: {
+        fontFamily: 'Hind',
+      },
+      body1: {
+        fontFamily: 'Hind',
+      },
+      body2: {
+        fontFamily: 'Hind',
+      },
+      button: {
+        fontFamily: 'Hind',
+      },
+      caption: {
+        fontFamily: 'Hind',
+      },
+      overline: {
+        fontFamily: 'Hind',
+      },
     },
-    h2: {
-      fontFamily: 'Montserrat',
-    },
-    h3: {
-      fontFamily: 'Montserrat',
-    },
-    h4: {
-      fontFamily: 'Montserrat',
-    },
-    h5: {
-      fontFamily: 'Montserrat',
-    },
-    h6: {
-      fontFamily: 'Montserrat',
-    },
-    subtitle1: {
-      fontFamily: 'Hind',
-    },
-    subtitle2: {
-      fontFamily: 'Hind',
-    },
-    body1: {
-      fontFamily: 'Hind',
-    },
-    body2: {
-      fontFamily: 'Hind',
-    },
-    button: {
-      fontFamily: 'Hind',
-    },
-    caption: {
-      fontFamily: 'Hind',
-    },
-    overline: {
-      fontFamily: 'Hind',
-    },
-  },
-});
+  });
--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './Themes';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 import Header from './sections/Header';
 import EventGrid from './sections/EventGrid';
 import EventPostPage from './EventPostPage.js';
 import reportWebVitals from './reportWebVitals';
+import { theme } from './Themes';
+
+import { Typography } from '@mui/material';
 
 const App = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/event-post" element={<EventPostPage />} />
-        {/* Insert other routes here */}
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Typography variant='h1'>yeh</Typography>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/event-post" element={<EventPostPage />} />
+          {/* Insert other routes here */}
+        </Routes>
+     </ThemeProvider>
     </Router>
   );
 };
 
 const Home = () => {
   return (
-    <div>
       <EventGrid />
-    </div>
   );
 };
 
-ReactDOM.render(<ThemeProvider theme={theme}>   <App />    </ThemeProvider>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+);
+
 reportWebVitals();
