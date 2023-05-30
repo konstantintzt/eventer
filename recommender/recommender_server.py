84f4cab86fc1edc311d22a2923dceac5aa31d108
wz-ml <willzhou97@ucla.edu>
2023-05-30 15:51:23 -0700

    Created flask server API and command-line functions for the recommender.

---
+++ recommender/recommender_server.py
@@ -0,0 +1,17 @@
+from sentence_transformers import SentenceTransformer
+from flask import Flask, redirect, url_for, request
+import numpy as np
+from utils import get_scores, get_location, compare
+model = SentenceTransformer('all-MiniLM-L6-v2')
+app = Flask(__name__)
+print("Model initialized.")
+
+@app.route('/recommend',methods = ['POST'])
+def recommend():
+   if request.method == 'POST':
+        history = request.json["history"]
+        available = request.json["available"]
+        scores = get_scores(model, history, available)
+        return scores
+
+app.run()
