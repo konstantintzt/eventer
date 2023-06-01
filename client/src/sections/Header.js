49204d4d014dc0ad06a2ad35d7f5a5dd32cbdb30
cqin22 <qincharles8@gmail.com>
2023-05-31 21:37:19 -0700

    Remove "skip login" and turn "login" functionality to "logout"

--- client/src/sections/Header.js
+++ client/src/sections/Header.js
@@ -54,7 +54,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
   },
 }));
-
 export default function SearchAppBar({ handleSearchSubmit }) {
   const [searchValue, setSearchValue] = useState('');
   const [menuAnchor, setMenuAnchor] = useState(null);
@@ -68,16 +67,18 @@ export default function SearchAppBar({ handleSearchSubmit }) {
     setMenuAnchor(event.currentTarget);
   };
 
-  const handleMenuClose = (path) => {
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    handleMenuClose('/');
+    window.location.reload()
+  };
 
+  const handleMenuClose = (path) => {
     setMenuAnchor(null);
-    console.log("path", path);
-    console.log("tocken", localStorage.getItem('token'))
     if (path === '/event-post' && !localStorage.getItem('token')) {
-      navigate('/'); //broken
+      navigate('/');
     } else {
       navigate(path);
-      console.log("path")
     }
   };
 
@@ -106,8 +107,8 @@ export default function SearchAppBar({ handleSearchSubmit }) {
             <MenuItem component={Link} to="/event/8fc1e95c-3ce5-4167-a75d-3accc5403c1c" onClick={() => handleMenuClose('/event/8fc1e95c-3ce5-4167-a75d-3accc5403c1c')}>
               Editor Pick
             </MenuItem>
-            <MenuItem component={Link} to="/login" onClick={() => handleMenuClose('/login')}>
-              Login
+            <MenuItem component={Link} onClick={handleLogout}>
+              Logout
             </MenuItem>
           </Menu>
 
