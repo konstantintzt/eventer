ec82332428a2e2c800b2413750b664beab29effc
cqin22 <qincharles8@gmail.com>
2023-06-01 18:44:17 -0700

    Completed issues 56 and 57 - created new header for different pages/removed editor pick

---
+++ client/src/EventPageHeader.js
@@ -0,0 +1,150 @@
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import { alpha } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 3, 1, 6.3),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
+export default function EventPageHeader({ handleSearchSubmit, showButtons }) {
+    const [searchValue, setSearchValue] = useState('');
+    const [menuAnchor, setMenuAnchor] = useState(null);
+    const navigate = useNavigate();
+
+    const handleSearchChange = (event) => {
+        setSearchValue(event.target.value);
+    };
+
+    const handleMenuOpen = (event) => {
+        setMenuAnchor(event.currentTarget);
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        handleMenuClose('/');
+        window.location.reload();
+    };
+
+    const handleMenuClose = (path) => {
+        setMenuAnchor(null);
+        console.log('path', path);
+        console.log('token', localStorage.getItem('token'));
+        if (path === '/event-post') {
+            navigate('/');
+        } else {
+            navigate(path);
+        }
+    };
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="sticky" color="primary" sx={{ width: '100%', left: 0, right: 0 }}>
+                <Toolbar sx={{ justifyContent: 'space-between' }}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        sx={{ mr: 2 }}
+                        onClick={handleMenuOpen}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        anchorEl={menuAnchor}
+                        open={Boolean(menuAnchor)}
+                        onClose={() => handleMenuClose('')}
+                    >
+                        <MenuItem component={Link} to="/event-post" onClick={() => handleMenuClose('/event-post')}>
+                            Post An Event
+                        </MenuItem>
+                        <MenuItem component={Link} onClick={handleLogout}>
+                            Logout
+                        </MenuItem>
+                    </Menu>
+
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                    >
+                        <Button color="inherit" component={Link} to="http://localhost:3000" sx={{ fontSize: '20px', marginTop: '2px' }}>
+                            Eventer
+                        </Button>
+                    </Typography>
+                    {showButtons && (
+                        <>
+                            <Button color="inherit" component={Link} to="/event-post" onClick={() => handleMenuClose('/event-post')}>
+                                Post
+                            </Button>
+                            <Search>
+                                <SearchIconWrapper>
+                                    <SearchIcon />
+                                </SearchIconWrapper>
+                                <StyledInputBase
+                                    placeholder=""
+                                    inputProps={{ 'aria-label': 'search' }}
+                                    value={searchValue}
+                                    onChange={handleSearchChange}
+                                />
+                            </Search>
+                            <Button color="inherit" onClick={() => handleSearchSubmit(searchValue)}>
+                                Search
+                            </Button>
+                        </>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
